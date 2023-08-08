7887e9cf1be1618270a7d73e6643a9df1e27e82e
Diogo Godoi <diogoogodoi@gmail.com>
2023-08-08 18:49:24 -0300

    TRACE

--- video.py
+++ video.py
@@ -12,8 +12,8 @@ class VideoRecord:
         self.cap = cv2.VideoCapture(0)
         
         self.expressoes = [1,-1]
-        arquivo_modelo = 'dcnn_modelo_01_expressoes.h5' # referente aos pesos
-        arquivo_modelo_json = 'dcnn_modelo_01_expressoes.json' # referente a arquitetura da Rede Neural
+        arquivo_modelo = 'cnn_expressoes.h5' # referente aos pesos
+        arquivo_modelo_json = 'cnn_expressoes.json' # referente a arquitetura da Rede Neural
         
         # Código para recepção do open (carregando o modelo salvo no item 6)
         json_file = open(arquivo_modelo_json, 'r')
@@ -23,14 +23,17 @@ class VideoRecord:
         # Fazendo a leitura do arquivo json para transformar esse para o modelo Tensorflow
         self.loaded_model = model_from_json(loaded_model_json)
         self.loaded_model.load_weights(arquivo_modelo)
+        print(self.loaded_model)
 
         self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
         self.running = True
+        self.emotionList = []
     
     def scoreEmotion(self,face):
         faces = self.face_cascade.detectMultiScale(face, 1.04, 5)
         lista_frames = []
         if len(faces) == 0:
+            print(faces)
             return 0
         else:
             for x,y,w,h in faces:
@@ -38,18 +41,18 @@ class VideoRecord:
                 toGray = cv2.cvtColor(face_cut,cv2.COLOR_RGB2GRAY)
                 resized = cv2.resize(toGray, (48,48))
                 normalized = resized.astype('float')/255
+                # print(normalized)
                 to_predict = np.expand_dims(normalized, -1)
-                lista_frames.append(normalized)
-                # cv2.imshow('webcam',to_predict)
+        #         lista_frames.append(normalized)
+                cv2.imshow('webcam',normalized)
 
                 try:                        
                     prediction = self.loaded_model.predict(to_predict)
                     print(prediction)
-                    # score = self.expressoes[int(np.argmax(prediction[-1]))]
-                    # return score
+        #             score = self.expressoes[int(np.argmax(prediction[-1]))]
+        #             # return score
                 except:
                     print('Erro')
-                    return 0
         
         
     def startRecording(self):
@@ -67,11 +70,11 @@ class VideoRecord:
         while self.running:
             ret,frame = self.startRecording()
             
-            self.score += self.scoreEmotion(frame)
+            self.scoreEmotion(frame)
             
             if not ret:
                 break
-            cv2.imshow('Webcam',frame)
+            # cv2.imshow('Webcam',frame)
             
             if cv2.waitKey(1) & 0xFF ==ord('q'):
                 break
